8c3deeaa4810902f4f571ba3abd5eb8c17ba83bb
shokhruh61 <shohruxmahmudov61@gmail.com>
2025-02-21 15:23:41 +0500

    type script

---
+++ compiled/index.js
@@ -0,0 +1,13 @@
+"use strict";
+// function squareArray(arr: number[]): number[] {
+//   return arr.map((num) => num * num);
+// }
+// console.log(squareArray([1, 2, 3, 4, 5]));
+// function saralash(arr: string[]): string[] {
+//   return arr.sort((a, b) => a.length - b.length);
+// }
+// console.log(saralash(["olma", "banan", "anor"]));
+// function Name(arr: string[], names: string): boolean {
+//   return arr.includes(names);
+// }
+// console.log(Name(["Ali", "Vali"], "Ali"));
